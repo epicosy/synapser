33a34371a1619ef2953f127203f025adcd6dc22c
epicosy <xtremedy@gmail.com>
2021-08-31 19:25:13 +0100

    install scripts updated

--- install.sh
+++ install.sh
@@ -6,15 +6,11 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get install -y postgresql libpq-dev && pip3 install -r requirements.txt
 [[ $? -eq 1 ]] && echo "[Error] Failed to install synapser dependencies." && exit 1 ;
 
-pip3 install setup.py
+pip3 install .
 [[ $? -eq 1 ]] && echo "[Error] Failed to install synapser." && exit 1 ;
 
 # Init DB
-sudo -u postgres -i
-/etc/init.d/postgresql start
-psql --command "CREATE USER synapser WITH SUPERUSER PASSWORD 'synapser123';"
-createdb synapser
-exit
+su -l postgres -c "/etc/init.d/postgresql start && psql --command \"CREATE USER synapser WITH SUPERUSER PASSWORD 'synapser123';\" && createdb synapser && exit"
 
 #Configs
 config_path="/etc/synapser"
--- install_py38.sh
+++ install_py38.sh
@@ -17,7 +17,7 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 [[ $? -eq 1 ]] && echo "[Error] Failed to set default version of Python 3 to 3.8.10" && exit 1 ;
 
 # Install pip for Python 3.8
-apt-get install -y python3-distutils
+apt-get install -y python3-distutils python3.8-dev
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 [[ $? -eq 1 ]] && echo "[Error] Failed to download pip for Python 3" && exit 1 ;
 python3 get-pip.py
