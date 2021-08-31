6a24f00abe78c5911921bc534fc7e5e0dedbfb96
epicosy <xtremedy@gmail.com>
2021-08-31 19:52:08 +0100

    stderr to stdout for some commands; config folder updated

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 # Setting Timezone
 DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 
-apt-get install -y postgresql libpq-dev && pip3 install -r requirements.txt
+apt-get install -y postgresql libpq-dev && pip3 install -r requirements.txt 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install synapser dependencies." && exit 1 ;
 
-pip3 install .
+pip3 install . 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install synapser." && exit 1 ;
 
 # Init DB
-su -l postgres -c "/etc/init.d/postgresql start && psql --command \"CREATE USER synapser WITH SUPERUSER PASSWORD 'synapser123';\" && createdb synapser"
+su -l postgres -c "/etc/init.d/postgresql start && psql --command \"CREATE USER synapser WITH SUPERUSER PASSWORD 'synapser123';\" && createdb synapser" 2>&1
 
 #Configs
 config_path="/etc/synapser"
-config_plugin_path="/etc/synapser/plugin"
-plugins_path="/var/lib/synapser/plugin/tool"
-mkdir -p $config_path && cp "config/synapser.yml" $config_path && mkdir -p $config_plugin_path && mkdir -p $plugins_path
+config_plugin_path="/etc/synapser/plugins.d"
+plugins_path="/var/lib/synapser/plugins/tool"
+mkdir -p $config_path && cp "config/synapser.yml" $config_path && mkdir -p $config_plugin_path && mkdir -p $plugins_path 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install synapser configs." && exit 1 ;
--- install_py38.sh
+++ install_py38.sh
@@ -16,8 +16,8 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 [[ $? -eq 1 ]] && echo "[Error] Failed to set default version of Python 3 to 3.8.10" && exit 1 ;
 
 # Install pip for Python 3.8
-apt-get install -y python3-distutils python3.8-dev
+apt-get install -y python3-distutils python3.8-dev 2>&1
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 [[ $? -eq 1 ]] && echo "[Error] Failed to download pip for Python 3" && exit 1 ;
-python3 get-pip.py
+python3 get-pip.py 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install pip for Python 3" && exit 1 ;
