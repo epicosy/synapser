4d2c74a0ea891b62220dd3a608f65382d4644069
epicosy <xtremedy@gmail.com>
2021-09-01 12:37:36 +0100

    redirect stderr to stdout

--- install_py38.sh
+++ install_py38.sh
@@ -3,12 +3,12 @@
 # Setting Timezone
 DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 
-apt-get update
-apt install -y software-properties-common
-add-apt-repository ppa:deadsnakes/ppa -y
+apt-get update 2>&1
+apt install -y software-properties-common 2>&1
+add-apt-repository ppa:deadsnakes/ppa -y 2>&1
 
 # Install Python 3.8.10
-apt install -y python3.8
+apt install -y python3.8 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install Python 3.8.10" && exit 1 ;
 
 # Set default version of Python 3 to 3.8.10
@@ -17,7 +17,7 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 
 # Install pip for Python 3.8
 apt-get install -y python3-distutils python3.8-dev 2>&1
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to download pip for Python 3" && exit 1 ;
 python3 get-pip.py 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install pip for Python 3" && exit 1 ;
